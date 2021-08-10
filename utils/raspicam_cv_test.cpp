2a9ff51d3a0e994ca48520dd8b75724f5500b22e
Joseph Peter <Josepholuwaseunpeter@yahoo.com>
2021-08-10 22:51:17 +0100

    solve git sync issues

--- utils/raspicam_cv_test.cpp
+++ utils/raspicam_cv_test.cpp
@@ -303,7 +303,7 @@ private:
             response_stream << obj;
             // boost::asio::write(sock, response);
             boost::asio::ip::udp::socket s(io_context_, 
-                    boost::asio::ip::udp::endpoint(
+                boost::asio::ip::udp::endpoint(
                         boost::asio::ip::udp::v4(), 0));
             boost::asio::ip::udp::endpoint destination(address, server_port);
             s.send_to(boost::asio::buffer(response.data() , response.size()), destination);
